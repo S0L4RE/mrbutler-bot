4a6fdce2309ad2f6ec36328cba811e46ca7b664f
Peter Urda <peter.urda@gmail.com>
2016-09-03 21:04:13 -0700

    Start by dup'ing code over to class

--- mrb/dice.py
+++ mrb/dice.py
@@ -101,3 +101,96 @@ def roll(input_string: str='') -> List[int]:
 
     # So we have valid input, within logical bounds. Roll em!
     return roll_dice(count=number_of_dice, sides=sides_per_die)
+
+
+class Dice(object):
+    """
+    Simple class, containing static operations to roll dice!
+    """
+
+    @staticmethod
+    def poor_mans_try_parse(input_string: str) -> bool:
+        """
+        Give any string, determine if the string can be converted to an `int`.
+
+        :param input_string: The input string to parse
+        :return: `True` if int(input_string) succeeds, `False` otherwise.
+        """
+
+        try:
+            _ = int(input_string)
+            return True
+        except ValueError:
+            return False
+
+    @staticmethod
+    def roll(input_string: str='') -> List[int]:
+        """
+        Given an input string, parse out and perform a "roll" of dice.
+
+        For example, inputting "2d20" would roll TWO dice with TWENTY sides
+        each.
+
+        :raises: `ValueError` on parse failure. Contains a readable message.
+
+        :param input_string: The string to parse
+        :return: A list of integers representing the rolled dice
+        """
+
+        delimiter = 'd'
+        dice_limit = 10
+        side_limit = 20
+        invalid_format_msg = "Expected format `NdM`! For example, `2d20`"
+
+        # Verify there is only one delimiter on input
+        if input_string.count(delimiter) != 1:
+            raise ValueError(invalid_format_msg)
+
+        # Split to get [number of dice, sides per dice]
+        input_split = input_string.split(delimiter)
+
+        # If any of these fail to parse out, bail out
+        if not all(Dice.poor_mans_try_parse(x) for x in input_split):
+            raise ValueError(invalid_format_msg)
+
+        # Parse values to ints
+        number_of_dice = int(input_split[0])
+        sides_per_die = int(input_split[1])
+
+        # Verify everything is above zero
+        if any(x <= 0 for x in [number_of_dice, sides_per_die]):
+            raise ValueError("You can't input non-positive numbers!")
+
+        # Raise exception on too many dice
+        if number_of_dice > dice_limit:
+            raise ValueError(
+                "You can't roll more than {} dice at once!".format(dice_limit)
+            )
+
+        # Raise exception on too many sides
+        if sides_per_die > side_limit:
+            raise ValueError(
+                "You can't roll a die with more than {} sides!".format(
+                    side_limit,
+                )
+            )
+
+        # So we have valid input, within logical bounds. Roll em!
+        return Dice.roll_dice(count=number_of_dice, sides=sides_per_die)
+
+    @staticmethod
+    def roll_dice(count: int, sides: int) -> List[int]:
+        """
+        Roll a give type of die or dice.
+
+        :param count: The number of dice to roll
+        :param sides: The number of sides per die
+        :return: A list of integers representing the rolled dice
+        """
+
+        result = []
+
+        for _ in range(count):
+            result.append(randint(1, sides))
+
+        return result
