da0a59c61ca716cc02857f6cc8c120e6bf24259b
Peter Urda <peter.urda@gmail.com>
2016-09-03 22:30:36 -0700

    Hashlib pass thru, probably dropping

--- mrb/audio/player.py
+++ mrb/audio/player.py
@@ -14,6 +14,9 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import hashlib
+import os
+
 from discord import VoiceClient
 
 
@@ -42,3 +45,38 @@ def run_audio_file(
 
         while player.is_playing():
             pass
+
+
+class Player(object):
+    def __init__(self):
+        # noinspection SpellCheckingInspection
+        self.hash_checks = {
+            'collar':
+                'b71e8ab0c3920370847543ef0e80a588f06b399dcd2857ee2fb1ee4e2ddb32f6',
+            'djkhaled':
+                '42b195ef28ecebcceed88faac89f805ad1369d7e19db55268e57adaf018a85d0',
+            'runorcurse':
+                'e3d98cdeca8c414e1361893d530514640e82f9cdfc735069b6f0557c4ad1dac8',
+        }
+
+        self.sound_files = {}
+
+        path = "{}/sounds/".format(
+            os.path.dirname(os.path.abspath(__file__))
+        )
+
+        for entry in os.scandir(path):
+            if not entry.is_file() or entry.name not in self.hash_checks:
+                continue
+
+            with open(entry.path, "rb") as sound_file:
+                data = sound_file.read()
+                if hashlib.sha256(data).hexdigest() != self.hash_checks[entry.name]:
+                    continue
+
+                self.sound_files[entry.name] = data
+
+
+if __name__ == '__main__':
+    x = Player()
+    _ = 1
