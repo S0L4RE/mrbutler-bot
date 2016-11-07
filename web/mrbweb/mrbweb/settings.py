b01a5a072f854421fd3e8bd7bfa602f394df41ce
Peter Urda <peter.urda@gmail.com>
2016-11-06 18:10:03 -0800

    Flip back to DEBUG based on MRB_ENV

--- web/mrbweb/mrbweb/settings.py
+++ web/mrbweb/mrbweb/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['MRB_DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True if os.environ.get('MRB_ENV', 'prod') == 'dev' else False
-DEBUG = True
+DEBUG = True if os.environ.get('MRB_ENV', 'prod') == 'dev' else False
 
 ALLOWED_HOSTS = []
 
