d85bf7c434596994de069a63bce8332f0e7b7f06
Peter Urda <peter.urda@gmail.com>
2016-11-13 17:07:26 -0800

    Render the challenge response from database

--- web/mrbweb/letsencrypt/views.py
+++ web/mrbweb/letsencrypt/views.py
@@ -14,20 +14,24 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from django.http import (
-    HttpResponse,
-    HttpResponseNotFound,
+from django.shortcuts import (
+    get_object_or_404,
+    render,
 )
+
+from .models import AcmeChallenge
 
 
 def detail(request, acme_data):
-    _ = request  # noqa
-    expected_request = 'lX8D7FNGoai8ktcHyAWo4I4zu6dAd9Fbz8aehEbAcJo'
+    acme_challenge = get_object_or_404(
+        AcmeChallenge,
+        challenge=acme_data,
+    )
 
-    if acme_data != expected_request:
-        return HttpResponseNotFound()
-
-    return HttpResponse(
-        'lX8D7FNGoai8ktcHyAWo4I4zu6dAd9Fbz8aehEbAcJo.'
-        'fL5DxaUDxd_IosDayHDs9HS4kGtc2XVCiQbOdyAg340'
+    return render(
+        request,
+        'letsencrypt/detail.html',
+        {
+            'response': acme_challenge.response
+        },
     )
