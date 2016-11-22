6fe94f173734c7ce597ab962e0e41739316de5cf
Peter Urda <peter.urda@gmail.com>
2016-11-22 17:58:37 -0500

    We need to update the default auth classes for drf

--- web/mrbweb/core/settings/base.py
+++ web/mrbweb/core/settings/base.py
@@ -138,6 +138,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
     'PAGE_SIZE': 10
 }
