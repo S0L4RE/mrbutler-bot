68738391ae638dab7e20ed86773deff4409be01d
Peter Urda <peter.urda@gmail.com>
2016-11-25 21:13:02 -0500

    Move settings to core, update refs

--- web/mrbweb/mrbweb/wsgi.py
+++ web/mrbweb/mrbweb/wsgi.py
@@ -14,21 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-"""
-WSGI config for mrbweb project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
-"""
-
 import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mrbweb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
