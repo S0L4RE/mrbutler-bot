d6fdc6d373b67b8a1ae08cc44c9edc7c518bde1e
Peter Urda <peter.urda@gmail.com>
2016-12-17 11:06:10 -0800

    Configure a mixin for these timestamps

---
+++ web/mrbweb/django_discord/models/mixins/created_updated_fields.py
@@ -0,0 +1,34 @@
+"""
+Copyright 2016 Peter Urda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from django.db import models
+
+
+class CreatedUpdatedFieldsMixin(models.Model):
+    created_ts = models.DateTimeField(
+        "Created Timestamp",
+        auto_now_add=True,
+        help_text='The timestamp for when this object was created',
+    )
+
+    updated_ts = models.DateTimeField(
+        "Updated Timestamp",
+        auto_now=True,
+        help_text='The timestamp for when this object was updated',
+    )
+
+    class Meta:
+        abstract = True
