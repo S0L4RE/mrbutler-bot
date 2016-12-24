2a13340e5fabb5770a55fd79ef29ca114fb17317
Peter Urda <peter.urda@gmail.com>
2016-12-23 21:06:44 -0500

    Change str method on guild

--- web/mrbweb/django_discord/models/guild.py
+++ web/mrbweb/django_discord/models/guild.py
@@ -54,4 +54,7 @@ class Guild(
     )
 
     def __str__(self):
-        return "Discord Guild <{}>".format(self.id)
+        return "Discord Guild <{0} -- {1}>".format(
+            self.id,
+            self.name,
+        )
