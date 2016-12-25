298e6e7de2f2cee7a95d572852c6fcc8a6e47bdf
Peter Urda <peter.urda@gmail.com>
2016-12-24 20:16:20 -0500

    Test the function from the user admin

---
+++ web/tests/unit/django_discord/admin/test_model_admins.py
@@ -0,0 +1,39 @@
+"""
+Copyright 2016 Peter Urda
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+from django.test import TestCase
+
+from django_discord.admin import UserAdmin
+from django_discord.models import User
+
+
+class TestUserAdmin(TestCase):
+    """Test the model admin for users"""
+
+    def test_username_for_django_admin(self):
+        """Verify the model admin is generating column values correctly"""
+        expected = 'user#1234'
+
+        user = User(
+            id='1234',
+            username='user',
+            discriminator='1234',
+            avatar='',
+        )
+
+        user_admin = UserAdmin(user, None)
+
+        self.assertEqual(expected, user_admin.username_for_django_admin(user))
